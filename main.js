32385ce66e0fb9cad1a24a1c616b37b95bdfee29
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-02-19 20:01:23 +0300

    Приехали

---
+++ main.js
@@ -0,0 +1,12 @@
+const asdasdasd = 10;
+
+console.log(asdasdasd);
+console.log(true);
+console.log(12312736123);
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
+console.log('123123 1 + 123123');
